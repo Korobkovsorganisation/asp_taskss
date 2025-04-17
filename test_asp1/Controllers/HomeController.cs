2ef7f816ea34542714b02d6ae431e116b1d1b38b
Андрей Максимович Коробков <144508844+Korobkov124@users.noreply.github.com>
2025-04-17 11:48:01 +0700

    do calc

--- test_asp1/Controllers/HomeController.cs
+++ test_asp1/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace test_asp1.Controllers
             return View();
         }
 
+        public IActionResult Calc()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
