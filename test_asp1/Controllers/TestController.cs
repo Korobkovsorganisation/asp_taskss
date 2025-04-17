0b4ec595677df4cd2e1916f36b4b561c5eb363df
Андрей Максимович Коробков <144508844+Korobkov124@users.noreply.github.com>
2025-04-17 10:53:04 +0700

    Добавьте файлы проекта.

---
+++ test_asp1/Controllers/TestController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using test_asp1.Models;
+using lib1;
+
+public class TestController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Greet(string name)
+    {
+        return Content($"Привет, {name}!");
+    }
+    public IActionResult UserInfo()
+    {
+        var user = new User { Name = "Иван Иванов", Age = 30 };
+        return View(user);
+    }
+    public IActionResult Calc(Calc calc)
+    {
+        switch (calc.operation)
+        {
+            case "+":
+                calc.result = MathFunctions.Add(calc.term1, calc.term2);
+                return View(calc);
+            case "-":
+                calc.result = MathFunctions.Substract(calc.term1, calc.term2);
+                return View(calc);
+            case "*":
+                calc.result = MathFunctions.Multiply(calc.term1, calc.term2);
+                return View(calc);
+            case "//":
+                calc.result = MathFunctions.Devide(calc.term1, calc.term2);
+                return View(calc);
+            default:
+                return View(calc);
+        }
+    }
+}
---
+++ test_asp1/Models/Calc.cs
@@ -0,0 +1,10 @@
+﻿namespace test_asp1.Models
+{
+    public class Calc
+    {
+        public int term1 { get; set; } = 0;
+        public int term2 { get; set; } = 0;
+        public string operation { get; set; } = "";
+        public double result { get; set; } = 0;
+    }
+}
